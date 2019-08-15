0d94876da0b64973c4a71b28583eefa11c713db2
Oliveira <moliveira@deloitte.com>
2019-08-15 20:49:37 -0300

    Focusing on layout improvements && Removing second route for better usability

--- src/App.svelte
+++ src/App.svelte
@@ -8,29 +8,35 @@
 
 <section class="component-app">
 	<div class="columns is-centered is-mobile">
-		<div class="column is-8-desktop is-10-tablet is-10-mobile">
+		<div class="column has-background-primary is-8-desktop is-10-tablet is-12-mobile">
 			<Router>
-				<div class="columns">
+				<!-- <div class="columns">
 					<div class="column">
 						<nav class="container-nav">
 							<Link to="/"><span class="has-text-weight-bold">Hoje</span></Link> 
 							<span class="has-text-white">/</span>
-							<Link to="previsao"><span class="has-text-weight-bold">Próximas Horas</span></Link>
-							<!-- <Link to="blog">Blog</Link> -->
+							<Link to="previsao"><span class="has-text-weight-bold">Próximas Horas</span></Link>							
 						</nav>
 						<p class="has-text-white">Clique em um dos links acima para alternar entre o tempo hoje e nas próximas horas.</p>
 					</div>
-				</div>				
-				<div class="columns">
+				</div> -->
+				<div class="columns has-background-grey-dark">
 					<div class="column">
+						<p class="has-text-white is-size-5"><span class="has-text-weight-bold has-text-danger">M</span>Weather</p>
+					</div>
+				</div>
+				<div class="columns">
+					<div class="column">						
 						<Route path="previsao" component="{ Forecast }" /> 
 						<Route path="/"><Home /></Route>
 					</div>
-					<!-- <Route path="blog/:id" component="{BlogPost}" />
-					<Route path="blog" component="{Blog}" /> -->
 				</div>
 			</Router>
-			<div class="has-text-white">Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+			<div class="columns">
+				<div class="column has-background-grey-dark">
+					<div class="has-text-white has-text-centered">Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+				</div>
+			</div>
 		</div>
 	</div>	
 </section>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,7 +2,7 @@
     import getWeatherIcon from '../helpers/getWeatherIcon.js';
     import { onMount } from 'svelte';
 
-    let date, data, text, src;
+    let weather, date, data, text, src;
 
     onMount(() => {
         const key = '554e9dc1d2e3e0ff2fedbe769b0a9288',
@@ -20,6 +20,24 @@
             text = desc.substr(0,1).toUpperCase() + desc.substr(1, desc.length);
         })
         .catch($error => { console.log("$error :: " + $error) });
+
+        fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + ID + '&appid=' + key + '&units=metric&cnt=21&lang=pt')
+        .then($result => { return $result.json() })
+        .then($data => {
+            weather = [];
+            $data.list.forEach($item => {                
+                const desc = $item.weather[0].description,
+                img = getWeatherIcon($item.weather[0].id),
+                text = desc.substr(0,1).toUpperCase() + desc.substr(1, desc.length);
+                weather.push({
+                    date: moment($item.dt_txt).format("LLL"),
+                    description: text,
+                    main: $item.main,
+                    src: img 
+                })
+            })
+        })
+        .catch($error => { console.log("$error :: " + $error) });
     })
 
 </script>
@@ -29,63 +47,57 @@
         width: 128px;
         height: auto;
     }
+    
+    .component-home .has-padding {
+        box-sizing: border-box;
+        padding: 0 20px;
+    }
 </style>
 
-<section class="component-home">
-    <hr>
-        <div class="columns">
-            <div class="column has-text-white">           
-                { #if !data } <p class="is-size-3 has-text-centered"> Carregando... </p> { /if }
-                { #if data }
-                    <p class="is-size-5"> { date } </p>
-                    <p class="is-size-4">Santo André, SP</p>
-                    <hr>
-                    <div class="columns has-text-centered has-text-white">
+<section class="component-home">    
+    <div class="columns">
+        <div class="column">           
+            { #if !data } <p class="is-size-4 has-text-centered has-text-white"> -- carregando -- </p> { /if }
+            { #if data }
+                <p class="is-size-5 has-text-white"> { date } </p>
+                <p class="is-size-4 has-text-white">Santo André, SP</p>                
+                <div class="columns has-text-centered has-text-white">
+                    <div class="column">
+                        { #if src }                                
+                            <img class="img" src={ src } alt="Ícone" >
+                            <p> { text } </p>
+                        { /if }                            
+                        <p class="is-size-2"> <span class="has-text-weight-bold"> { data.main.temp } </span> C°</p>
+                    </div>
+                </div>
+                { #if weather }
+                    <div class="columns has-background-white">
                         <div class="column">
-                            { #if src }                                
-                                <img class="img" src={ src } alt="Ícone" >
-                                <p> { text } </p>
-                            { /if }                            
-                            <p class="is-size-2"> <span class="has-text-weight-bold"> { data.main.temp } </span> C°</p>
+                            { #each weather as forecast }
+                                <div class="columns has-padding is-mobile is-vcentered">
+                                    <div class="column is-6">
+                                        <p> { forecast.date } </p>
+                                    </div>
+                                    <div class="column is-6 has-text-right">
+                                        <p class="is-size-4"><span class="has-text-weight-bold"> { forecast.main.temp } </span> C° </p>
+                                        <p class="has-text-grey"> { forecast.description } </p>
+                                    </div>
+                                </div>
+                            { /each }
                         </div>
                     </div>
-                    <div class="columns has-text-centered has-text-white">
-                        <div class="column is-6">
-                            <p class="is-size-4 is-size-3-mobile">Min</p>
-                            <p class="is-size-3 is-size-2-mobile"> <span class="has-text-weight-bold"> { data.main.temp_min } </span> C°</p>
-                        </div>
-                        <div class="column is-6">
-                            <p class="is-size-4 is-size-3-mobile">Max</p>
-                            <p class="is-size-3 is-size-2-mobile"> <span class="has-text-weight-bold"> { data.main.temp_max } </span> C°</p>
-                        </div>
-                    </div>
-                    <!-- <article class="message">
-                        <div class="message-header"><p> Santo André, SP ~ { date } </p></div>
-                        <div class="message-body has-text-centered has-text-white">                    
-                            <div class="columns">
-                                <div class="column">
-                                    <p class="is-size-5"> { text } </p>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="columns">
-                                <div class="column">
-                                    <p> Min </p>
-                                    <p> <span class="is-size-3 has-text-weight-bold"> { data.main.temp_min } </span> C°</p>
-                                </div>
-                                <div class="column">
-                                    <p> Atual </p>
-                                    <p> <span class="is-size-3 has-text-weight-bold"> { data.main.temp } </span> C°</p>
-                                </div>
-                                <div class="column">
-                                    <p> Max </p>
-                                    <p> <span class="is-size-3 has-text-weight-bold"> { data.main.temp_max } </span> C°</p>
-                                </div>
-                            </div>                    
-                        </div>
-                    </article> -->
                 { /if }
-            </div>
+                <!-- <div class="columns has-text-centered has-text-white">
+                    <div class="column is-6">
+                        <p class="is-size-4 is-size-3-mobile">Min</p>
+                        <p class="is-size-3 is-size-2-mobile"> <span class="has-text-weight-bold"> { data.main.temp_min } </span> C°</p>
+                    </div>
+                    <div class="column is-6">
+                        <p class="is-size-4 is-size-3-mobile">Max</p>
+                        <p class="is-size-3 is-size-2-mobile"> <span class="has-text-weight-bold"> { data.main.temp_max } </span> C°</p>
+                    </div>
+                </div> -->                
+            { /if }
         </div>
-    <hr>
+    </div>    
 </section>
